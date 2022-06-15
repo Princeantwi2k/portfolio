122eade967d42b9c5158e55be1158f5803df9bc1
princeantwi2k <antwi2k@gmail.com>
2022-06-16 03:17:27 +0800

    added content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './Components/Navbar/Navbar';
 import './App.css';
 import Intro from './Components/Intro/Intro';
+import Services from './Components/Service/Services';
 
 function App() {
   return (
     <div className="App">
     <Navbar />
     <Intro />
+    <Services />
     </div>
   );
 }
