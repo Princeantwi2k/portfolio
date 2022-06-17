7a8c65d8ca28dcb4a4e1dedf975033bcb461685d
princeantwi2k <antwi2k@gmail.com>
2022-06-17 13:09:35 +0800

    footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Footer.css";
+import Wave1 from "../../img/wave1.png";
+import Insta from "@iconscout/react-unicons/icons/uil-instagram";
+import Facebook from "@iconscout/react-unicons/icons/uil-facebook";
+import Github from "@iconscout/react-unicons/icons/uil-github";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <img src={Wave1} alt="" style={{ width: "100%" }} />
+      <div className="f-content">
+        <span>Antwi2k@gmail.com</span>
+        <div className="f-icon">
+          <Insta color="white" size="3rem" />
+          <Facebook color="white" size="3rem" />
+          <Github color="white" size="3rem" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
