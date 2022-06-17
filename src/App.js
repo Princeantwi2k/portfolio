1eca80c2e022799d3194a6969b54f459c36a8bc8
princeantwi2k <antwi2k@gmail.com>
2022-06-17 13:09:49 +0800

    added App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import Navbar from './Components/Navbar/Navbar';
 import './App.css';
 import Intro from './Components/Intro/Intro';
 import Services from './Components/Service/Services';
-
+import Experience from './Components/Experience/Experience';
+import Work from './Components/Work/Work'
+import Portfolio from './Components/Portfolio/Portfolio';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div className="App">
     <Navbar />
     <Intro />
     <Services />
+    <Experience />
+    <Work />
+    <Portfolio />
+    <Contact />
+    <Footer />
     </div>
   );
 }
